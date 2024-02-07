439a3b7dfbd794928f4f0ae192b5826425227ecd
nano <marianosenduk@gmail.com>
2024-02-07 19:39:56 +0700

    add feature edit product(finished)

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,13 +10,32 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    static long counter = 0;
 
     public Product create(Product product) {
+        product.setProductId(Long.toString(++counter));
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product edit(Product productEdited) {
+        String idProductEdited = productEdited.getProductId();
+        Product product = findById(idProductEdited);
+        product.setProductName(productEdited.getProductName());
+        product.setProductQuantity(productEdited.getProductQuantity());
+        return product;
     }
 }
