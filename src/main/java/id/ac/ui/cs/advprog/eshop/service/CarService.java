7119d736787fd917c528ac057a9a38aabe7a7356
nano <marianosenduk@gmail.com>
2024-02-24 18:29:24 +0700

    fix file naming

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -1,7 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.service;
-
 import id.ac.ui.cs.advprog.eshop.model.Car;
-
 import java.util.List;
 
 public interface CarService {
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,10 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.service;
-
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
